ae66c3daf9b9fbf7d693432f52eee2762c373b69
BrunoZRR <bruno.miguel.zarrabe.ricoy@students.iaac.net>
2024-02-07 15:54:09 -0600

    1
    progress

--- src/main.js
+++ src/main.js
@@ -13,3 +13,21 @@ import '@/styles/toggle-styles.css'
 
 // Create Vue appliction with App.vue component and inject it into element with id 'app'
 createApp(App).mount("#app")
+
+// this is my first line in javascript
+//console.log("Hello World")
+
+const doc = document.documentElement;
+//console.log (doc)
+
+// Change style of doc form js
+doc.style.background = "beige"
+doc.style.fontFamily = "monospace"
+
+// access particular elements
+const navbar = document.querySelector("#navbar")
+//console.log(nav)
+nav.innerHTML = "something"
+nav.style.fontSize = "12px"
+
+const sidebar = document.querySelector("#sidebar")
